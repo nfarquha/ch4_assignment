c642f5d034f7943a26afe3eb73fc4f604a04a705
nfarquha <nfarquha@uncc.edu>
2016-10-13 01:27:21 -0400

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,20 +15,22 @@ class MoviesController < ApplicationController
   end
   
  def create
-  @movie = Movie.create!(params[:movie])
-  flash[:notice] = "#{@movie.title} was successfully created."
-  redirect_to movies_path
- end
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
  
  def edit
   @movie = Movie.find params[:id]
  end
 
 def update
-  @movie = Movie.find params[:id]
-  @movie.update_attributes!(params[:movie])
-  flash[:notice] = "#{@movie.title} was successfully updated."
-  redirect_to movie_path(@movie)
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
 end
 
 def destroy
